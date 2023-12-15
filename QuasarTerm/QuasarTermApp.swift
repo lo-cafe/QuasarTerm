0a9e7f553d76df1d8b5eca7a42ad5c2f3d670ea1
Daniel <daniel.inama02@gmail.com>
2023-12-15 15:30:10 +0100

    Initial Commit

---
+++ QuasarTerm/QuasarTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuasarTermApp.swift
+//  QuasarTerm
+//
+//  Created by daniel on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct QuasarTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
