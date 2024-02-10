bccca77115809fa68cbb4de038be416dcb5f668f
Daniel <daniel.inama02@gmail.com>
2024-02-10 11:21:49 +0100

    Make settings nicer, add ability to set custom shell with arguments

---
+++ QuasarTerm/Views/Settings/GeneralSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSettings.swift
+//  QuasarTerm
+//
+//  Created by daniel on 10/02/24.
+//
+
+import SwiftUI
+
+struct GeneralSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralSettings()
+}
